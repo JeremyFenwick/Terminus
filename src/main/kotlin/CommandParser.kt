b5fa55f31d5e33c6bc86d27adb12dbdcd2007418
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-08-01 00:06:36 +1000

    codecrafters submit [skip ci]

--- src/main/kotlin/CommandParser.kt
+++ src/main/kotlin/CommandParser.kt
@@ -1,6 +1,9 @@
+import kotlin.system.exitProcess
+
 object CommandParser {
   fun parse(input: String) {
     when (input) {
+      "exit 0" -> exitProcess(0)
       else -> println("${input}: command not found")
     }
   }
