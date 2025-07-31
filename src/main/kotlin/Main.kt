766add2da05077ba33a2c0a15055a9f7148e22d9
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-08-01 00:05:07 +1000

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
-  // Uncomment this block to pass the first stage
-  print("$ ")
-  val userInput = readln() // Wait for user input
-  CommandParser.parse(userInput) // Parse the input command
+  while (true) {
+    print("$ ")
+    val userInput = readln() // Wait for user input
+    CommandParser.parse(userInput) // Parse the input command
+  }
 }
