7e103d9bf3e6baaf334c898cc10a19f171060e59
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-08-01 13:45:27 +1000

    codecrafters submit [skip ci]

--- src/main/kotlin/Commander.kt
+++ src/main/kotlin/Commander.kt
@@ -1,11 +1,55 @@
 import kotlin.system.exitProcess
 
-object Commander {
-  fun execute(command: String) {
-    when {
-      command == "exit 0" -> exitProcess(0)
-      command.startsWith("echo ") -> println(command.removePrefix("echo "))
-      else -> println("${command}: command not found")
+enum class CommandType {
+  ECHO,
+  EXIT,
+  TYPE,
+  UNKNOWN;
+
+  companion object {
+    fun fromInput(input: String): CommandType {
+      return when (input) {
+        "echo" -> ECHO
+        "exit" -> EXIT
+        "type" -> TYPE
+        else -> UNKNOWN
+      }
     }
+  }
+}
+
+data class Command(val type: CommandType, val content: String, val subType: CommandType)
+
+object Commander {
+  fun execute(command: Command) {
+    when (command.type) {
+      CommandType.ECHO -> println(command.content)
+      CommandType.EXIT -> exitProcess(0)
+      CommandType.TYPE -> {
+        when (command.subType) {
+          CommandType.UNKNOWN -> println("${command.content}: not found")
+          else -> println("${command.subType.toString().lowercase()} is a shell builtin")
+        }
+      }
+
+      CommandType.UNKNOWN -> println("${command.content}: command not found")
+    }
+  }
+
+  fun parse(input: String): Command {
+    val parts = input.split(" ")
+    if (parts.isEmpty()) {
+      return Command(CommandType.UNKNOWN, "", CommandType.UNKNOWN)
+    }
+    val commandType = CommandType.fromInput(parts[0])
+    val subCommandType =
+        if (parts.size > 1) CommandType.fromInput(parts[1]) else CommandType.UNKNOWN
+    val content =
+        when {
+          commandType == CommandType.UNKNOWN -> parts.joinToString(" ")
+          subCommandType != CommandType.UNKNOWN -> parts.drop(2).joinToString(" ")
+          else -> parts.drop(1).joinToString(" ")
+        }
+    return Command(commandType, content, subCommandType)
   }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ fun main() {
   while (true) {
     print("$ ")
     val userInput = readln() // Wait for user input
-    Commander.execute(userInput) // Parse the input command
+    val command = Commander.parse(userInput) // Parse the input command
+    Commander.execute(command) // Parse the input command
   }
 }
