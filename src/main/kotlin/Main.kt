2468b31d922bf5d97db77c0147c3ddc02e911ac9
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-08-02 20:04:30 +1000

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ fun main() {
   val pathFolders = System.getenv("PATH")?.split(":") ?: listOf()
   val directories = AvailablePrograms(pathFolders)
   val shell = Shell("$ ")
-  //  val reader = BufferedReader(InputStreamReader(System.`in`))
 
   while (true) {
     val line = shell.readLine() // Read a line from the shell
