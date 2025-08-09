2383722a5f412bb7ce62a9d2014a45e1474eb86f
Jeremy Fenwick <jeremy.fenwick@gmail.com>
2025-08-09 21:55:40 +1000

    codecrafters submit [skip ci]

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,16 @@ const val HISTORY_LIMIT = 1000 // Maximum number of history entries
 
 fun main() = runBlocking {
   val pathFolders = System.getenv("PATH")?.split(":") ?: listOf()
+
   val programs = AvailablePrograms(pathFolders)
   val shell = Shell("$ ", CommandType.commandList() + programs.executables.keys.toList())
-  val commander = Commander(shell)
+  // We start the commander with a prior history if specified by spoofing the correct command
+  val commander =
+      Commander(shell).also {
+        val histFile = System.getenv("HISTFILE")
+        val command = CommandGenerator.parseInput("history -r $histFile")
+        if (histFile != null) it.run(command, programs)
+      }
 
   while (true) {
     val rawLine = shell.readLine()
